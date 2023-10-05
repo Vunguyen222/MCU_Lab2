8c7ae8103ccdfee042408552a132553ce570b228
Vunguyen222 <vunguyenking@gmail.com>
2023-10-05 17:02:21 +0700

    Save change software timer

---
+++ STM32_LAB2/Core/Src/software_timer.c
@@ -0,0 +1,26 @@
+/*
+ * software_timer.c
+ *
+ *  Created on: 5 thg 10, 2023
+ *      Author: DELL
+ */
+
+#include "software_timer.h"
+
+int timer0_counter = 0;
+int timer0_flag = 0;
+int timer_cycle = 10;
+
+void setTimer0(int duration){
+	timer0_counter = duration / timer_cycle;
+	timer0_flag = 0;
+}
+
+void timer_run(){
+	if(timer0_counter > 0){
+		timer0_counter--;
+		if(timer0_counter <= 0){
+			timer0_flag = 1;
+		}
+	}
+}
